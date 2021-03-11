c2a59a628ec0178dfd5b7f24590e56590bcd87a1
suhail <suhail609@gmail.com>
2021-03-12 03:02:00 +0530

    can see individual posts of users

--- routes/stories.js
+++ routes/stories.js
@@ -45,6 +45,26 @@ router.get('/', ensureAuth, async (req, res) => {
     }
 })
 
+//@desc   Show single story
+//@route GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+
+    try {
+        story = await Story.findById({_id: req.params.id}).populate('user').lean()
+        
+        if(!story) {
+            return res.render('error/404')
+        }
+        res.render('stories/show', {
+            story
+        })
+
+    } catch (error) {
+        console.error(error)
+        res.render('error/404')
+    }
+})
+    
 //@desc   Show edit page
 //@route GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
@@ -113,6 +133,25 @@ router.delete('/:id', ensureAuth, async (req, res) => {
         return res.render('error/500')
     }
 })
+
+//@desc     Show user stories
+//@route    GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: 'public'
+        }).populate('user').lean()
+
+        res.render('stories/index',{
+            stories
+        })
+    } catch (error) {
+        console.error(error)
+        res.render('error/500')
+    }
+})
+
 
 
 module.exports = router;
